c39bef93cfa4a380871a10d5c32a01d29eb69dec
ahmedmamdoh3142 <96729668+ahmedmamdoh3142@users.noreply.github.com>
2026-04-16 19:33:12 +0200

    edit img3

--- Backend/app.js
+++ Backend/app.js
@@ -34,7 +34,7 @@ app.use((req, res, next) => {
 });
 
 // ✅ خدمة الملفات الثابتة للصور المرفوعة
-app.use('/uploads', express.static(path.join(__dirname, '..', 'uploads')));
+app.use('/uploads', express.static('/var/www/real-estate/uploads'));
 
 // Middleware لمعالجة JSON
 app.use(express.json({ limit: '50mb' }));
